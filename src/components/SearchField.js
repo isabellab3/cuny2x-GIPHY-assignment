29832c5060e96dcdb50d14194c80f2c9a763c76a
Anthony <anthony@Apples-MacBook-Pro.local>
2020-01-09 10:34:11 -0500

    added searchfield

---
+++ src/components/SearchField.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class SearchField extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      searchInput:""
+    }
+  }
+  handleInput = (e) => {
+    this.setState({searchInput:e.target.value})
+  }
+  handleSubmit = () => {
+    // Use input to make api call here
+  }
+  render(){
+    return <div className="searchField">
+              <form>
+                <p>Search Gifs</p>
+                <input type="text" value={this.state.searchInput} onChange={this.handleInput}></input>
+                <button onSubmit={this.handleSubmit}>Submit</button>
+              </form>
+           </div>
+  }
+}
+
+export default SearchField
