e79f7b50f39f426ae711fb34594a4d0432dcbaf4
Will Lu <willorwgl@gmail.com>
2020-01-09 12:44:53 -0500

    implement handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SearchField from './components/SearchField.js'
+import SearchField from './components/Dashboard.js'
 
 function App() {
   return (
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,24 +1,33 @@
 import React, {Component} from 'react';
+import axios from "axios"
 
+
+
+const API_KEY = "tmtF2QRwHh7tnPxTZp5sxWf4FSX5DcBQ"
 class SearchField extends Component{
   constructor(props){
     super(props)
     this.state = {
-      searchInput:""
+      searchInput:"",
+      gifs: []
     }
   }
   handleInput = (e) => {
     this.setState({searchInput:e.target.value})
   }
-  handleSubmit = () => {
-    // Use input to make api call here
+  handleSubmit = async (e) => {
+    e.preventDefault(); 
+    const { searchInput } = this.state;
+    const endpoint = `http://api.giphy.com/v1/gifs/search?q=${searchInput}&api_key=${API_KEY}`
+    const gifs = await axios.get(endpoint)
+    this.setState({gifs})
   }
   render(){
     return <div className="searchField">
-              <form>
+              <form onSubmit={this.handleSubmit}>
                 <p>Search Gifs</p>
                 <input type="text" value={this.state.searchInput} onChange={this.handleInput}></input>
-                <button onSubmit={this.handleSubmit}>Submit</button>
+                <button type="submit">Submit</button>
               </form>
            </div>
   }
